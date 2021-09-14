290a078b37ef3945f77b07c154d610871cbee62a
Lizzy <lebogang@eblocks.co.za>
2021-09-14 16:03:29 +0200

    [Lizzy] adding register classes and interfaces

---
+++ af.assessment.api.test/Controllers/RegisterControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace af.assessment.api.test.Controllers
+{
+    class Class1
+    {
+    }
+}
---
+++ af.assessment.api/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using af.assessment.api.Services;
+
+namespace af.assessment.api.Controllers
+{
+    public class RegisterController
+    {
+        private readonly IRegisterService _registerService;
+
+        public RegisterController(IRegisterService registerService)
+        {
+            _registerService = registerService;
+        }
+    }
+}
